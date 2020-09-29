94ddf1f46a01029997d7a27fd0460a0280795d45
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2020-09-28 22:04:52 -0300

    Criado o ContatoController

--- src/main/java/com/github/darlansantos/agendaapi/AgendaApiApplication.java
+++ src/main/java/com/github/darlansantos/agendaapi/AgendaApiApplication.java
@@ -1,13 +1,7 @@
 package com.github.darlansantos.agendaapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.github.darlansantos.agendaapi.domain.Contato;
-import com.github.darlansantos.agendaapi.repository.ContatoRepository;
 
 @SpringBootApplication
 public class AgendaApiApplication {
@@ -16,16 +10,16 @@ public class AgendaApiApplication {
 		SpringApplication.run(AgendaApiApplication.class, args);
 	}
 	
-	@Bean
-	public CommandLineRunner commandLineRunner(
-			@Autowired ContatoRepository contatoRepository) {
-		return args -> {
-			Contato contato = new Contato();
-			contato.setNome("Darlan");
-			contato.setEmail("darlanparreiras@gmail.com");
-			contato.setFavorito(false);
-			contatoRepository.save(contato);						
-		};
-	}
+//	@Bean
+//	public CommandLineRunner commandLineRunner(
+//			@Autowired ContatoRepository contatoRepository) {
+//		return args -> {
+//			Contato contato = new Contato();
+//			contato.setNome("Darlan");
+//			contato.setEmail("darlanparreiras@gmail.com");
+//			contato.setFavorito(false);
+//			contatoRepository.save(contato);						
+//		};
+//	}
 	
 }
---
+++ src/main/java/com/github/darlansantos/agendaapi/controller/ContatoController.java
@@ -0,0 +1,64 @@
+package com.github.darlansantos.agendaapi.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.darlansantos.agendaapi.domain.Contato;
+import com.github.darlansantos.agendaapi.repository.ContatoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/contatos")
+public class ContatoController {
+	
+	private final ContatoRepository contatoRepository; 
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Contato save(@RequestBody Contato contato) {
+		return contatoRepository.save(contato);
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<Contato> findById(@PathVariable Integer id) {
+		  Optional<Contato> contato = contatoRepository.findById(id);
+		  return contato;
+	}
+	
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<Contato> findAll() {
+		  return contatoRepository.findAll();
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Integer id) {
+		contatoRepository.deleteById(id);
+	}
+	
+	@PatchMapping("{id}/favorito")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void favorite(@PathVariable Integer id, @RequestBody Boolean favorito) {
+		Optional<Contato> contato = contatoRepository.findById(id);
+		contato.ifPresent(c -> {
+			c.setFavorito(favorito);
+			contatoRepository.save(c);
+		});
+	}
+	
+}
